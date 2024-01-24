d9332c4c48b64f48a0fb0c7e2c2ec35fbf23d254
CeoMontana <2004ssnicoo@gmail.com>
2024-01-24 05:38:21 -0500

    Hero

---
+++ src/Components/Hero.jsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <main className='flex flex-col items-center justify-center'>
+        <h1 className='font-bold text-2xl '>Backend Excellence</h1>
+        <p className='font-light tracking-tighter text-xl'>Somos un agencia dedicada a brindarte el mejor servicio de desarollos de sitios web.</p>
+       <div className='flex items-center justify-center w-full '>
+        <button className='p-2 hover:scale-110 shadow-xl shadow-black rounded-lg bg-slate-400 border-2 border-black text-black hover:text-white hover:bg-slate-900 hover:border-blue-500'>
+            <Link href="/">Consultar Ahora</Link>
+        </button>
+       </div>
+    </main>
+  )
+}
+
+export default Hero
--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/Components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="font-bold text-2xl">Hello</h1>
-    </main>
+    <>
+      <Hero />
+    </>
   );
 }

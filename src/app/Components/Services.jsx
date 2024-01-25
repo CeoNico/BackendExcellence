660a71b1a9f5a26351ddf316553331a5aa4ef4ad
CeoMontana <2004ssnicoo@gmail.com>
2024-01-25 03:42:01 -0500

    Card component and web section

--- src/app/Components/Services.jsx
+++ src/app/Components/Services.jsx
@@ -1,30 +1,46 @@
 import React from "react";
-import insightImg from "../../../public/insight.png";
+import responsiveImg from "../../../public/responsive.jpg";
+import seoImg from "../../../public/seo.png";
+import webdevImg from "../../../public/webdev.jpg";
 import Image from "next/image";
+import Card from "./Card";
 
 const Services = () => {
   return (
-    <div className="h-screen w-full px-8">
-      <h1 className="text-2xl font-bold text-center p-2">Nuestros Servicios</h1>
-      <div className="">
-        <p className="font-light text-xl">
-          En backend excellence ofrecemos varios servicios para que tu empresa
-          sigue para adelnte. Usamos la tecnoliga mas moderna para no quedarse
-          atras
-        </p>
-        <div className="flex flex-col lg:grid-cols-3 border-2 items-center justify-center gap-3">
-          <div className="text-center p-2">
-            <h2>Diseño Web</h2>
+    <div className="h-screen w-full lg:h-fit p-16">
+      <h2 className="text-center text-2xl font-bold p-4">Nosotros</h2>
+      <div className="grid grid-cols-1 items-center gap-10 lg:grid-cols-3">
+        <div className="flex flex-col items-center">
+          <Card imgSrc={webdevImg}>
+            <h2 className="font-bold text-2xl">Desarollo Principal</h2>
             <p>
-              Nuestro diseño web se trata de convertir tu idea en algo visible
-              en el mundo digital. Esto es muy importante ya que cada dia la
-              tecnologia avanza mas y mas y el internet es la cosa mas usada en
-              el mundo.
+              Nuestro desarollo principal cuenta con varias tecnicas para
+              asegurar que tu sitio web sea a tu gusto junto con cumplir tu idea
+              principal. Tenemos en cuenta tambien desarollar un sitio web facil
+              para navegar, rapido y con el mejor diseno UX posible!
             </p>
-            <Image src={insightImg} />
-          </div>
-          <div>This is the second Service</div>
-          <div>This is the third Service</div>
+          </Card>
+        </div>
+        <div className="flex flex-col items-center">
+          <Card imgSrc={seoImg}>
+            <h2 className="font-bold text-2xl">SEO Optimizacion</h2>
+            <p>
+              Es clave que tu sitio web tenga un ranking de SEO alto. Lo que
+              hace esto es poner tu sitio web arriba de los restos de los
+              resultados cuando buscando algo en el internet.
+            </p>
+          </Card>
+        </div>
+        <div className="flex flex-col items-center">
+          <Card imgSrc={responsiveImg}>
+            <h2 className="font-bold text-2xl">Sitio Responsive</h2>
+            <p>
+              Cuando desarollamos tu sitio web es importante para nosotros que
+              todos los aparatos con acceso al internet pueden ver y interatucar
+              con tu sitio web. Esto significa que sea telefono, tablet o compu
+              todos van a tener acceso a tu sitio web.
+            </p>
+          </Card>
         </div>
       </div>
     </div>
